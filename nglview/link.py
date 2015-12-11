571215ec8a9c14e7eaf6552f2d5ebf39bb46e158
Hai Nguyen <hainm.comp@gmail.com>
2015-12-11 13:45:45 -0500

    add animation_link_to_mpl.ipynb notebook

--- nglview/link.py
+++ nglview/link.py
@@ -1,11 +1,6 @@
 '''temp module
 '''
 
-from matplotlib.widgets import AxesWidget
-import numpy as np
-
-import nglview
-
 
 def link_mpl(ngl_widget, ax, data, **dot_kwd):
     """link ngl_widget to Axes
@@ -21,13 +16,15 @@ def link_mpl(ngl_widget, ax, data, **dot_kwd):
         import matplotlib.pyplot as plt
         from pytraj import view
         import pytraj as pt
-        from nglview.matplotlib_link import link_mpl
+
+        from nglview.link import link_mpl
         import nglview
 
         fig = plt.figure(figsize=(8, 6))
         ax = fig.add_subplot(111)
         ax.set_xlabel('frame number')
         ax.set_ylabel('End to end distance (Angstrom)')
+
         traj = pt.datafiles.load_tz2()
         data = pt.radgyr(traj, '@CA')
         ax.plot(data)
@@ -35,9 +32,15 @@ def link_mpl(ngl_widget, ax, data, **dot_kwd):
         # convert pytraj's Trajectory to nglview.Trajectory
 
         ntraj = pt.view.to_nglview(traj)
+
         tv = nglview.TrajectoryViewer(ntraj)
-        link_mpl(tv, ax)
+        link_mpl(tv, ax, data)
+        tv
     """
+    from matplotlib.widgets import AxesWidget
+    import numpy as np
+    import nglview
+
     data = np.asarray(data)
 
     if data.ndim == 1:
@@ -46,23 +49,24 @@ def link_mpl(ngl_widget, ax, data, **dot_kwd):
         x, y = data
     else:
         raise ValueError('support 1D or 2D array')
-    
-    lineh = ax.axhline(ax.get_ybound()[0], c="green")
-    linev = ax.axvline(ax.get_xbound()[0], c="green")
-    dot, = ax.plot([],[], 'o', c=dot_kwd.get('color', 'red'),
-                               ms=dot_kwd.get('ms', 10))
+
+    #lineh = ax.axhline(ax.get_ybound()[0], c="green")
+    #linev = ax.axvline(ax.get_xbound()[0], c="green")
+    lineh = ax.axhline(ax.get_ybound()[0], c='white', alpha=0.1)
+    linev = ax.axvline(ax.get_xbound()[0], c='white', alpha=0.1)
+    dot, = ax.plot([], [], 'o', c=dot_kwd.get('color', 'red'),
+                   ms=dot_kwd.get('ms', 10))
 
     ngl_widget.isClick = False
 
-    
     def onclick(event):
         linev.set_xdata((event.xdata, event.xdata))
         lineh.set_ydata((event.ydata, event.ydata))
         ngl_widget.isClick = True
         ngl_widget.frame = index
 
-    def frame_changed( self ):
-        nglview.TrajectoryViewer._frame_changed( ngl_widget )
+    def frame_changed(self):
+        nglview.TrajectoryViewer._frame_changed(ngl_widget)
         if ngl_widget.isClick is True:
             ngl_widget.isClick = False
         else:
@@ -70,9 +74,9 @@ def link_mpl(ngl_widget, ax, data, **dot_kwd):
             dot.set_ydata((y[ngl_widget.frame]))
             linev.set_xdata(ax.get_ybound()[0])
             lineh.set_ydata(ax.get_xbound()[0])
-            
+
     ngl_widget._frame_changed = frame_changed
-        
+
     axes_widget = AxesWidget(ax)
     axes_widget.connect_event('button_release_event', onclick)
-    return ngl_widget
+    # return ngl_widget
