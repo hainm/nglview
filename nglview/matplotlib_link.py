4722f4dee905509840ee504204663dca033201ee
Hai Nguyen <hainm.comp@gmail.com>
2015-12-11 13:02:02 -0500

    update link_mpl

--- nglview/matplotlib_link.py
+++ nglview/matplotlib_link.py
@@ -7,12 +7,14 @@ import numpy as np
 import nglview
 
 
-def link_mpl(ngl_widget, ax):
+def link_mpl(ngl_widget, ax, data, **dot_kwd):
     """link ngl_widget to Axes
 
     Parameters
     ----------
     ngl_widget : nglview.TrajectoryViewer
+    ax : matplotlib Axes
+    data : original data, 1D or 2D array-like
 
     Examples
     --------
@@ -36,35 +38,41 @@ def link_mpl(ngl_widget, ax):
         tv = nglview.TrajectoryViewer(ntraj)
         link_mpl(tv, ax)
     """
-    # adapted from Arose's code
+    data = np.asarray(data)
 
-    # TODO: not work yet, why?
-    x_data, y_data = ax.get_lines()[0].get_data()
-
-    lineh = ax.axhline(ax.get_ybound()[0])
-    linev = ax.axvline(ax.get_xbound()[0])
-    dot, = ax.plot([], [], 'o', c='red', ms=10)
+    if data.ndim == 1:
+        x, y = range(len(data)), data
+    elif data.ndim == 2:
+        x, y = data
+    else:
+        raise ValueError('support 1D or 2D array')
+    
+    lineh = ax.axhline(ax.get_ybound()[0], c="green")
+    linev = ax.axvline(ax.get_xbound()[0], c="green")
+    dot, = ax.plot([],[], 'o', c=dot_kwd.get('color', 'red'),
+                               ms=dot_kwd.get('ms', 10))
 
     ngl_widget.isClick = False
 
+    
     def onclick(event):
-        print('onclick')
         linev.set_xdata((event.xdata, event.xdata))
         lineh.set_ydata((event.ydata, event.ydata))
         ngl_widget.isClick = True
         ngl_widget.frame = index
 
-    def frame_changed(self):
-        nglview.TrajectoryViewer._frame_changed(ngl_widget)
+    def frame_changed( self ):
+        nglview.TrajectoryViewer._frame_changed( ngl_widget )
         if ngl_widget.isClick is True:
             ngl_widget.isClick = False
         else:
-            dot.set_xdata((x_data[ngl_widget.frame]))
-            dot.set_ydata((y_data[ngl_widget.frame]))
+            dot.set_xdata((x[ngl_widget.frame]))
+            dot.set_ydata((y[ngl_widget.frame]))
             linev.set_xdata(ax.get_ybound()[0])
             lineh.set_ydata(ax.get_xbound()[0])
-
+            
     ngl_widget._frame_changed = frame_changed
-
+        
     axes_widget = AxesWidget(ax)
     axes_widget.connect_event('button_release_event', onclick)
+    return ngl_widget
